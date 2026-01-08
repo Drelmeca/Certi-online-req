cd4a09c5770539fe58b5159296e2a090b6008e1d
Drelmeca <150115404+Drelmeca@users.noreply.github.com>
2026-01-08 21:27:42 +0800

    Update Edit.vue

--- resources/js/pages/User/Edit.vue
+++ resources/js/pages/User/Edit.vue
@@ -14,7 +14,7 @@ const users = reactive({
     id: null,
     name: '',
     email: '',
-    status: 0,
+    status: '',
     role: 2,
     password: '',
     password_confirmation: '',
@@ -80,6 +80,7 @@ onMounted(() => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
+                        
                         <h1 class="text-2xl font-bold mb-4">Create User</h1>
                         <form @submit.prevent="createusers">
                             <div class="mb-4">
